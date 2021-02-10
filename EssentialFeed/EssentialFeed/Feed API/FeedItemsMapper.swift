5bffea695259702a440ad13ac3cb41400936a1e1
Marko Engelman <engleman.marko@gmail.com>
2021-02-10 11:02:59 +0100

    Move all 'FeedItems' mapping logic into 'FeedItemsMapper' due to preparation for implementation of 'ImageCommentsLoader'.

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -9,11 +9,22 @@ final class FeedItemsMapper {
 		let items: [RemoteFeedItem]
 	}
 	
-	static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+	private enum Error: Swift.Error {
+		case invalidData
+	}
+	
+	static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
 		guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-			throw RemoteFeedLoader.Error.invalidData
+			throw Error.invalidData
 		}
 		
-		return root.items
+		return root.items.toModels()
+	}
+}
+
+// MARK: - Private
+private extension Array where Element == RemoteFeedItem {
+	func toModels() -> [FeedImage] {
+		return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
 	}
 }
--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -26,26 +26,23 @@ public final class RemoteFeedLoader: FeedLoader {
 			
 			switch result {
 			case let .success((data, response)):
-				completion(RemoteFeedLoader.map(data, from: response))
+				completion(Self.map(data, from: response))
 				
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
-	
-	private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-		do {
-			let items = try FeedItemsMapper.map(data, from: response)
-			return .success(items.toModels())
-		} catch {
-			return .failure(error)
-		}
-	}
 }
 
-private extension Array where Element == RemoteFeedItem {
-	func toModels() -> [FeedImage] {
-		return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
+// MARK: - Private
+private extension RemoteFeedLoader {
+	static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+		do {
+			let items = try FeedItemsMapper.map(data, from: response)
+			return .success(items)
+		} catch {
+			return .failure(Error.invalidData)
+		}
 	}
 }
